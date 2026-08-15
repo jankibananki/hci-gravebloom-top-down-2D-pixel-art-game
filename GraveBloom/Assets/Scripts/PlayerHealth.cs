2b13c072451b7f6f17af16aa991cb0fbfa1a5aca
Jana Markovic <janamarkovic77@gmail.com>
2026-08-15 09:18:28 +0200

    dodat health bar

---
+++ GraveBloom/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 5;
+    public int currentHealth;
+
+    public Image healthBarImage;
+
+    // Redom:
+    // 0 HP, 1 HP, 2 HP, 3 HP, 4 HP, 5 HP
+    public Sprite[] healthSprites;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthBar();
+    }
+
+    //privremeno dok ne dodam skeletoni
+    void Update()
+    {
+        if (Keyboard.current == null)
+            return;
+
+        if (Keyboard.current.hKey.wasPressedThisFrame)
+        {
+            TakeDamage(1);
+        }
+
+        if (Keyboard.current.jKey.wasPressedThisFrame)
+        {
+            Heal(1);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        // Ako shield postoji i aktivan je,
+        // on prima udarac umesto healtha.
+        PlayerShield shield = GetComponent<PlayerShield>();
+
+        if (shield != null && shield.IsShieldActive())
+        {
+            shield.HitShield();
+            return;
+        }
+
+        currentHealth -= damage;
+
+        currentHealth = Mathf.Clamp(
+            currentHealth,
+            0,
+            maxHealth
+        );
+
+        UpdateHealthBar();
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+
+        currentHealth = Mathf.Clamp(
+            currentHealth,
+            0,
+            maxHealth
+        );
+
+        UpdateHealthBar();
+    }
+
+    void UpdateHealthBar()
+    {
+        if (healthBarImage == null)
+            return;
+
+        healthBarImage.sprite = healthSprites[currentHealth];
+    }
+
+    void Die()
+    {
+        Debug.Log("Player died!");
+    }
+}
